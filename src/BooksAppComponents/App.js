9ab8372e8af46dcd69b6325e366375bbb8188d50
serbulas1 <serbulas1@gmail.com>
2022-12-25 17:45:41 +0100

    Finish book app with passing references as props

--- src/BooksAppComponents/App.js
+++ src/BooksAppComponents/App.js
@@ -1,15 +1,40 @@
 import { useState } from "react";
 import BookCreate from "./BookCreate";
+import BookList from "./BookList";
 
 function App() {
-  const [book, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
 
   const createBook = (title) => {
-    console.log("Add book with", title);
+    const id = Math.round(Math.random() * 9999);
+    const updatedBooks = [...books, { id, title }];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <h1>Reading list</h1>
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
